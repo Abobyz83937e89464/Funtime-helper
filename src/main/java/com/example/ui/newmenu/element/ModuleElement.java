ac285d85628119fc3cf57db5c01d1313eed818e5
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-04-27 10:25:50 +0300

    Create ModuleElement.java

---
+++ src/main/java/com/example/ui/newmenu/element/ModuleElement.java
@@ -0,0 +1,17 @@
+package com.example.ui.newmenu.element;
+
+import net.minecraft.client.gui.DrawContext;
+import java.awt.Color;
+
+public class ModuleElement {
+
+    // Эта пустышка будет отвечать за отрисовку самих панелек читов (Aura, Sprint и т.д.)
+    
+    public void render(DrawContext context, float x, float y /*, Module module */) {
+        // Здесь будет код из оригинального ModuleElement, когда добавим модули и настройки
+    }
+
+    public float getHeight() {
+        return 40f; // Временная высота
+    }
+}
