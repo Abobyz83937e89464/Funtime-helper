55993237dcb6a2221505d725779d28f54450d221
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-04-29 15:36:45 +0300

    Update ScaleUtil.java

--- src/main/java/com/example/util/render/ScaleUtil.java
+++ src/main/java/com/example/util/render/ScaleUtil.java
@@ -3,7 +3,27 @@ package com.example.util.render;
 import net.minecraft.client.MinecraftClient;
 
 public class ScaleUtil {
+
     public static float getScale() {
         return (float) MinecraftClient.getInstance().getWindow().getScaleFactor();
+    }
+
+    public static int getScaledWidth() {
+        return MinecraftClient.getInstance().getWindow().getScaledWidth();
+    }
+
+    public static int getScaledHeight() {
+        return MinecraftClient.getInstance().getWindow().getScaledHeight();
+    }
+
+    /**
+     * Переводит scaled-координаты мыши в реальные пиксельные.
+     */
+    public static double toRealX(double scaledX) {
+        return scaledX * getScale();
+    }
+
+    public static double toRealY(double scaledY) {
+        return scaledY * getScale();
     }
 }
