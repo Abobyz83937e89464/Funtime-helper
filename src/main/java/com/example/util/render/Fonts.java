f79a5776dcb6f851ab34547461b9265c84b5ba43
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-05-01 19:42:36 +0300

    Update Fonts.java

--- src/main/java/com/example/util/render/Fonts.java
+++ src/main/java/com/example/util/render/Fonts.java
@@ -2,7 +2,6 @@ package com.example.util.render;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.util.Identifier;
 
 public class Fonts {
 
@@ -11,20 +10,10 @@ public class Fonts {
     public static TextRenderer REGULAR;
 
     public static void init() {
-        BOLD = MinecraftClient.getInstance().textRenderer;
-        MEDIUM = MinecraftClient.getInstance().textRenderer;
+        // В 1.21.4 getFontManager() убран, используем стандартный textRenderer
+        // Кастомные шрифты через json грузятся автоматически через fontRenderer по Identifier
+        BOLD    = MinecraftClient.getInstance().textRenderer;
+        MEDIUM  = MinecraftClient.getInstance().textRenderer;
         REGULAR = MinecraftClient.getInstance().textRenderer;
-
-        try {
-            var fontManager = MinecraftClient.getInstance().getFontManager();
-            BOLD = fontManager.getRenderer(Identifier.of("modid", "inter_bold"));
-            MEDIUM = fontManager.getRenderer(Identifier.of("modid", "inter_medium"));
-            REGULAR = fontManager.getRenderer(Identifier.of("modid", "inter_regular"));
-        } catch (Exception e) {
-            // fallback на стандартный если что-то пошло не так
-            BOLD = MinecraftClient.getInstance().textRenderer;
-            MEDIUM = MinecraftClient.getInstance().textRenderer;
-            REGULAR = MinecraftClient.getInstance().textRenderer;
-        }
     }
 }
