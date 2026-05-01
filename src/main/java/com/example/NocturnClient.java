a221bc8dee722aded7ffb86865c9cca1f7420dbc
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-05-01 20:48:42 +0300

    Update NocturnClient.java

--- src/main/java/com/example/NocturnClient.java
+++ src/main/java/com/example/NocturnClient.java
@@ -3,7 +3,6 @@ package com.example;
 import net.fabricmc.api.ClientModInitializer;
 
 public class NocturnClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
         System.out.println("[Nocturn Client] Loaded!");
