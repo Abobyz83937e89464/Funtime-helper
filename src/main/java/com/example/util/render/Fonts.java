afc636169907b3895c2f491ba9c7b81ae7f6ff53
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-05-03 11:23:42 +0300

    Update Fonts.java

--- src/main/java/com/example/util/render/Fonts.java
+++ src/main/java/com/example/util/render/Fonts.java
@@ -4,7 +4,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 
 public class Fonts {
-
     public static TextRenderer get() {
         return MinecraftClient.getInstance().textRenderer;
     }
