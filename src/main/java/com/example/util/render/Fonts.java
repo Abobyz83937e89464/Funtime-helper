d7e58c21500d09b2a48f1d3c6cb4fbee9d00dca4
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-05-03 11:42:52 +0300

    Update Fonts.java

--- src/main/java/com/example/util/render/Fonts.java
+++ src/main/java/com/example/util/render/Fonts.java
@@ -7,4 +7,6 @@ public class Fonts {
     public static TextRenderer get() {
         return MinecraftClient.getInstance().textRenderer;
     }
+    public static int width(String text) { return get().getWidth(text); }
+    public static int height()           { return get().fontHeight; }
 }
