32e054271395d8f1edf504ab37a4aabeecefbb2c
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-05-05 17:21:49 +0300

    Update NocturnClient.java

--- src/main/java/com/example/NocturnClient.java
+++ src/main/java/com/example/NocturnClient.java
@@ -10,7 +10,6 @@ public class NocturnClient implements ClientModInitializer {
     public void onInitializeClient() {
         System.out.println("[Nocturn Client] Loaded!");
 
-        // Инициализируем шейдер после создания GL контекста
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
             DrawHelper.initShader();
         });
