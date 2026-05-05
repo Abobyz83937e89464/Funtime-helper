bad0b2ea63ff9d1a95e8c64c75eda40469e4c025
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-05-05 16:54:47 +0300

    Update NocturnClient.java

--- src/main/java/com/example/NocturnClient.java
+++ src/main/java/com/example/NocturnClient.java
@@ -2,11 +2,6 @@ package com.example;
 
 import com.example.util.render.DrawHelper;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
 
 public class NocturnClient implements ClientModInitializer {
 
@@ -14,18 +9,8 @@ public class NocturnClient implements ClientModInitializer {
     public void onInitializeClient() {
         System.out.println("[Nocturn Client] Loaded!");
 
-        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
-            .registerReloadListener(new SimpleSynchronousResourceReloadListener() {
-
-                @Override
-                public Identifier getFabricId() {
-                    return Identifier.of("nocturn-client", "drawhelper");
-                }
-
-                @Override
-                public void reload(ResourceManager manager) {
-                    DrawHelper.init(manager);
-                }
-            });
+        // Регистрируем шейдер через CoreShaderRegistrationCallback
+        // (должно быть ДО первого рендер-фрейма)
+        DrawHelper.registerShaders();
     }
 }
